2ee850e80459a1ef27ca704cb037946fc59d0379
JuanDeossa <juandeossa.nc@gmail.com>
2022-10-23 13:38:27 -0500

    Html Layout with React Components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import { TodoCounter } from "./TodoCounter";
+import { TodoSearch } from "./TodoSearch";
+import { TodoList } from "./TodoList";
+import { TodoItem } from "./TodoItem";
+import { CreateTodoButton } from "./CreateTodoButton";
 // import './App.css';
 
+import React from "react";
+
 function App(props) {
+  const toDoS = [
+    {text:"clean room",completed:false},
+    {text:"play games",completed:false},
+    {text:"eat pizza",completed:false},
+    {text:"sleep",completed:false},
+  ]
   return (
-    <div></div>
+    <React.Fragment>
+      <TodoCounter/>
+      <TodoSearch/>
+      <TodoList>
+        {toDoS.map(toDo=>(
+          <TodoItem key={toDo.text} text={toDo.name}/>
+        ))}
+      </TodoList>
+      <CreateTodoButton/>
+    </React.Fragment>
   );
 }
 
---
+++ src/TodoCounter.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TodoCounter() {
+  return (
+    <h2>You have completed all your tasks</h2>
+  )
+}
+
+export {TodoCounter}
