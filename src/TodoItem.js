9053bb19b7f3e66cde25eaa4a90f567d4ccdf3a5
JuanDeossa <juandeossa.nc@gmail.com>
2022-10-23 17:46:18 -0500

    onClick event

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,11 +3,23 @@ import "./TodoItem.css"
 
 function TodoItem(props) {
   const {text,completed}=props
+  function deleteTask(task) {
+    console.log(`You deleted the task: ${task}`);
+  }
+  function completeTask(task) {
+    console.log(`You completed the task: ${task}`);
+  }
   return (
     <li className='TodoItem'>
-        <span className='TodoItem__delete'>🅧</span>
+        <span
+        className='TodoItem__delete'
+        onClick={()=>deleteTask(text)}>
+        🅧</span>
         <p className={`TodoItem__text ${completed && "TodoItem__text--ok"}`}>{text}</p>
-        <span className='TodoItem__ok'>☑</span>
+        <span 
+        className='TodoItem__ok'
+        onClick={()=>completeTask(text)}
+        >☑</span>
     </li>
   )
 }
