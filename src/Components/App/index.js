bbe9d3d767dffdbbcd1b27385e6d3415599f4b20
JuanDeossa <juandeossa.nc@gmail.com>
2022-10-24 10:13:54 -0500

    local storage V1

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,16 +1,34 @@
 import React from "react";
 import { AppUI } from "./AppUI.js";
 
-const defaultTasks = [
-  {text:"clean room",completed:false},
-  {text:"play games",completed:false},
-  {text:"eat pizza",completed:false},
-  {text:"sleep",completed:false},
-]
+// const defaultTasks = [
+//   {text:"clean room",completed:false},
+//   {text:"play games",completed:false},
+//   {text:"eat pizza",completed:false},
+//   {text:"sleep",completed:false},
+// ]
+
 
 function App() {
+  //Localstorage
+  const localStorageVersion = "TASKS_V1"
+  const localStorageTasks = localStorage.getItem(localStorageVersion)
+  let parsedTasks
+  if (!localStorageTasks) {
+    localStorage.setItem(localStorageVersion,"[]")
+    parsedTasks=[]
+  } else {
+    parsedTasks=JSON.parse(localStorageTasks)
+  }
+
+  function saveTasks(newTasks) {
+    const strTasks=JSON.stringify(newTasks)
+    localStorage.setItem(localStorageVersion,strTasks)
+    setTask(newTasks)
+  }
+  //
   const [searchValue,setSearchValue] = React.useState("")
-  const [tasks,setTask] = React.useState(defaultTasks)
+  const [tasks,setTask] = React.useState(parsedTasks)
   const totalTasks = tasks.length
   const completedTasks = tasks.filter(task=>!!task.completed).length
   let searchedTasks=[]
@@ -33,7 +51,7 @@ function App() {
     } else {
       newTasks[index].completed=true
     }
-    setTask(newTasks)
+    saveTasks(newTasks)
   }
 
   function deleteTask(taskValue) {
@@ -42,7 +60,7 @@ function App() {
     if (index>=0) {
       newTasks.splice(index,1)
     }
-    setTask(newTasks)
+    saveTasks(newTasks)
   }
 
   return (
