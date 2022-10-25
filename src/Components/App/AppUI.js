d63541dff3afbe2b8059bd4e1a4af9dcfe5b4801
JuanDeossa <juandeossa.nc@gmail.com>
2022-10-25 10:26:11 -0500

    React context TodoList (using useContext)

--- src/Components/App/AppUI.js
+++ src/Components/App/AppUI.js
@@ -5,33 +5,35 @@ import { TodoList } from "../TodoList/index.js";
 import { TodoItem } from "../TodoItem/index.js";
 import { CreateTodoButton } from "../CreateTodoButton/index.js";
 import './App.css'
+import React from "react";
 
 function AppUI(){
+    const {
+        error,
+        loading,
+        deleteTask,
+        toggleTaskState,
+        searchedTasks
+    }=React.useContext(taskContext)
     return(
     <>
         <TodoCounter/>
         <TodoSearch/>
-        <taskContext.Consumer>
-            {({error,loading,deleteTask,toggleTaskState,searchedTasks})=>{
-              return(
-                <TodoList>
-                {error && <p className="stateMessage">error...</p>}
-                {loading && <p className="stateMessage">loading...</p>}
-                {(!loading && !searchedTasks.length) && <p className="stateMessage">Create a new task &#10549;</p>}
-                {searchedTasks.map(task=>(
-                    <TodoItem
-                    key={task.text}
-                    text={task.text}
-                    completed={task.completed}
-                    toggleTask={()=>toggleTaskState(task.text)}
-                    deleteTask={()=>deleteTask(task.text)}
-                    />
-                ))}
-                </TodoList>
-            )}}
-        </taskContext.Consumer>
+        <TodoList>
+        {error && <p className="stateMessage">error...</p>}
+        {loading && <p className="stateMessage">loading...</p>}
+        {(!loading && !searchedTasks.length) && <p className="stateMessage">Create a new task &#10549;</p>}
+        {searchedTasks.map(task=>(
+            <TodoItem
+            key={task.text}
+            text={task.text}
+            completed={task.completed}
+            toggleTask={()=>toggleTaskState(task.text)}
+            deleteTask={()=>deleteTask(task.text)}
+            />
+        ))}
+        </TodoList>
         <CreateTodoButton/>
     </>)
 }
-
 export {AppUI}
